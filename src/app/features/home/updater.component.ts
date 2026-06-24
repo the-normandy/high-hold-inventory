35557f275a6c79c0e350ec2740bcccdb9fbb6b6f
the-normandy <astarianott@gmail.com>
2026-06-24 11:41:27 -0300

    Add update checker in a component

---
+++ src/app/features/home/updater.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { check, Update } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+    selector: 'app-updater',
+    templateUrl: 'updater.component.html',
+    styles: `:host { @apply flex-1; }`,
+    imports: [MatIconModule, MatButtonModule, MatProgressSpinnerModule]
+})
+export class UpdaterComponent implements OnInit {
+
+    ngOnInit() {
+        this.verifyUpdate();
+    }
+
+    async verifyUpdate() {
+        const update = await check();
+        if (update) {
+            this.updateData = update;
+            this.version.set(update.version);
+            this.shouldRenderUpdateRequest.set(true);
+        }
+    }
+
+    renderMessage = signal<string | null>(null);
+    shouldRenderUpdateRequest = signal<boolean>(false);
+    shouldRenderUpdateProgress = signal<boolean>(false);
+    version = signal<string | null>(null);
+    updateData: Update | null = null;
+    private updating = false;
+
+    dismissUpdate() {
+        this.shouldRenderUpdateRequest.set(false);
+        this.shouldRenderUpdateProgress.set(false);
+    }
+
+    async startUpdate() {
+        if (this.updating) {
+            return;
+        }
+
+        this.updating = true;
+        let downloaded = 0;
+        let contentLength = 0;
+        this.shouldRenderUpdateRequest.set(false);
+        this.shouldRenderUpdateProgress.set(true);
+
+        if (!this.updateData) { 
+            this.renderMessage.set('An unknown error occurred.');
+            return;
+         }
+
+         try {
+            await this.updateData.downloadAndInstall((event) => {
+                switch(event.event) {
+                    case 'Started': {
+                        contentLength = event.data.contentLength ?? 0;
+                        this.renderMessage.set('Starting update...');
+                        break;
+                    }
+                    case 'Progress': {
+                        downloaded += event.data.chunkLength;
+
+                        const mbDownloaded = (downloaded / 1024 / 1024).toFixed(1);
+                        const mbTotal = (contentLength / 1024 / 1024).toFixed(1);
+
+                        this.renderMessage.set(`Downloading: ${mbDownloaded} of ${mbTotal}`)
+                        break;
+                    }
+                    case 'Finished': {
+                        this.renderMessage.set("Download finished. Installing...");
+                        break;
+                    }
+                }
+            });
+         } catch(error) {
+            console.error(error);
+            this.renderMessage.set('Update failed.');
+         } finally {
+            this.updating = false;
+         }
+
+        await relaunch();
+    }
+}
