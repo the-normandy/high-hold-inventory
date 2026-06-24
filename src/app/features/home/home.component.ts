8448e7561b56701701e0d760be3a6985d6892de0
the-normandy <astarianott@gmail.com>
2026-06-24 19:32:01 -0300

    Adjust component to use updater component rather than service

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,19 +2,15 @@ import { Component, OnInit } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { RouterLink } from "@angular/router";
-import { testUpdater } from "./updater.service";
+import { UpdaterComponent } from "./updater.component";
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.component.html',
     styles: `:host { @apply flex-1; }`,
     imports: [
-        MatCardModule, MatButtonModule, RouterLink
+        MatCardModule, MatButtonModule, RouterLink, UpdaterComponent
     ]
 })
-export class HomeComponent implements OnInit {
-
-    ngOnInit() {
-        testUpdater();
-    }
+export class HomeComponent {
 }
