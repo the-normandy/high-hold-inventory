e4406039c7dd12751591d65b5a70123928de002d
the-normandy <astarianott@gmail.com>
2026-06-25 22:43:07 -0300

    Improve UI with title, Back button, and primary action button

--- src/app/features/actions/actions.component.ts
+++ src/app/features/actions/actions.component.ts
@@ -1,13 +1,13 @@
 import { Component } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { RouterLink } from "@angular/router";
-import { MatAnchor } from "@angular/material/button";
+import { MatAnchor, MatButtonModule } from "@angular/material/button";
 
 @Component({
     selector: 'app-actions',
     templateUrl: 'actions.component.html',
     styles: ':host { @apply flex-1; }',
-    imports: [MatCardModule, RouterLink, MatAnchor]
+    imports: [MatCardModule, RouterLink, MatAnchor, MatButtonModule]
 })
 export class ActionsComponent {
 
