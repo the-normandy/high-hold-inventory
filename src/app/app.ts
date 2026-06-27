9390393bfca8002cfc7dcdc725e8c30d6f5eaeee
the-normandy <astarianott@gmail.com>
2026-06-27 14:28:05 -0300

    Add version to app window

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { ThemeStore } from './core/theme/theme.store';
+import { getVersion } from '@tauri-apps/api/app'
+import { getCurrentWindow } from '@tauri-apps/api/window'
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,23 @@ import { ThemeStore } from './core/theme/theme.store';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('High Hold Storehouse');
+  version = signal<string>('');
+  protected readonly title = computed(() => `Storehouse`);
   mode = inject(ThemeStore);
+
+  constructor() {
+    this.updateWindow();
+  }
+  
+  async getCurrentVersion() {
+    const version = await getVersion();
+    this.version.set(version);
+  }
+
+  async updateWindow() {
+    await this.getCurrentVersion();
+    await getCurrentWindow().setTitle(`Storehouse ${this.version()}`);
+  }
 
   themeChange() {
     this.mode.isDark.update(value => !value);
