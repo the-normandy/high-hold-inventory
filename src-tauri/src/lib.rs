fc1abbba5f200794cc9ae40d8f6f66be0917071b
the-normandy <astarianott@gmail.com>
2026-06-28 11:58:20 -0300

    Add tauri plugin FS dependency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,7 @@
 pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_updater::Builder::new().build())
+    .plugin(tauri_plugin_fs::init())
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
