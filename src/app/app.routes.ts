16b5a13ffebb5e2a158206878b8d4bb24f07dbb8
the-normandy <astarianott@gmail.com>
2026-07-04 19:58:52 -0300

    Add data management route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ActionsComponent } from './features/actions/actions.component';
 import { HomeComponent } from './features/home/home.component';
 import { InventoryComponent } from './features/inventory/inventory.component';
+import { DataComponent } from './features/data/data.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'new', component: ActionsComponent},
-    {path: 'new/:mode', component: InventoryComponent}
+    {path: 'new/:mode', component: InventoryComponent},
+    {path: '/data', component: DataComponent}
 ];
