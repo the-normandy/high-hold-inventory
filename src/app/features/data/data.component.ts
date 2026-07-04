9e996c1175d940ae494ae6f6bbc1d2473b8bce72
the-normandy <astarianott@gmail.com>
2026-07-04 20:26:48 -0300

    Add scaffolding for data management

--- src/app/features/data/data.component.ts
+++ src/app/features/data/data.component.ts
@@ -1,15 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 import { MatTreeModule } from "@angular/material/tree";
+import { TreeNode } from './data.model'
 
 @Component({
     selector: 'app-data',
     templateUrl: 'data.component.html',
     styles: `:host { @apply flex-1; }`,
     imports: [
-        MatButtonModule, MatTreeModule
+        MatButtonModule, MatTreeModule, MatIconModule
     ]
 })
 export class DataComponent {
 
+    treeNodes = signal<TreeNode[]>([]);
 }
---
+++ src/app/features/data/data.model.ts
@@ -0,0 +1,5 @@
+export interface TreeNode {
+    key: string;
+    path: string[];
+    children?: TreeNode[];
+}
