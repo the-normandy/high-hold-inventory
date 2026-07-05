5ddaa8b5d65dc6f37ce412e525e2c6ca626b0e69
the-normandy <astarianott@gmail.com>
2026-07-05 18:10:13 -0300

    Switch icon dependency to Material Symbols

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { ThemeStore } from './core/theme/theme.store';
+import { MatIconRegistry } from '@angular/material/icon';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideAppInitializer(() => { const theme = inject(ThemeStore); })
+    provideAppInitializer(() => { 
+      const theme = inject(ThemeStore); 
+      inject(MatIconRegistry).setDefaultFontSetClass('material-symbols-outlined')
+    })
   ]
 };
