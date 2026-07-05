ef04965fb5a161c2fca16af81344a3d6ae1e8203
the-normandy <astarianott@gmail.com>
2026-07-05 00:56:38 -0300

    Add method to fetch Tree data

--- src/app/core/data/data.store.ts
+++ src/app/core/data/data.store.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ItemData } from "./item.model";
 import { PricesFile } from "./data.service"
+import { TreeNode } from "../../features/data/data.model";
 
 @Injectable({
     providedIn: 'root'
@@ -17,4 +18,33 @@ export class DataStore {
 
     items = {} as Record<string, ItemData[]>;
     craftData = {} as Record<string, Record<string, ItemData[]>>;
+
+    getTree(): TreeNode[] {
+        const materialRoot: TreeNode = {
+            name: 'Materials',
+            path: ['materials'],
+            children: Object.keys(this.items).map(key => ({
+                name: key,
+                path: ['materials', key]
+            }))
+        };
+
+        const craftRoot: TreeNode = {
+            name: 'Craft',
+            path: ['craft'],
+            children: Object.entries(this.craftData).map(([craft, categories]) => ({
+                name: craft,
+                path: ['craft', craft],
+                children: Object.keys(categories).map(category => ({
+                    name: category,
+                    path: ['craft', craft, category]
+                }))
+            }))
+        };
+
+        return [
+            materialRoot,
+            craftRoot
+        ];
+    }
 }
