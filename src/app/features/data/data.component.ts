2c115c9a2d539b32eed767f78295b8ca06b45147
the-normandy <astarianott@gmail.com>
2026-07-05 23:17:19 -0300

    Add data snapshot loading based on generic json structure

--- src/app/features/data/data.component.ts
+++ src/app/features/data/data.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from "@angular/core";
+import { Component, computed, inject, OnInit, signal } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatTreeModule } from "@angular/material/tree";
@@ -6,6 +6,8 @@ import { TreeNode } from './data.model'
 import { DataStore } from "../../core/data/data.store";
 import { MatDividerModule } from "@angular/material/divider";
 import { RouterLink } from "@angular/router";
+import { ItemData } from "../../core/data/item.model";
+import { PricesFile } from "../../core/data/data.service";
 
 @Component({
     selector: 'app-data',
@@ -20,20 +22,47 @@ import { RouterLink } from "@angular/router";
 export class DataComponent implements OnInit {
 
     ngOnInit(): void {
+        this.dataSnapshot.set({
+            schema: structuredClone(this.data.schema),
+            craft: structuredClone(this.data.craftData),
+            materials: structuredClone(this.data.items),
+        });
         this.treeData.set(this.data.getTree());
     }
 
     private readonly data = inject(DataStore);
     readonly childrenAccessor = (node: TreeNode) => node.children ?? [];
     hasChild = (_: number, node: TreeNode) => !!node.children && node.children.length > 0;
+    dataSnapshot = signal<PricesFile | null>(null);
     treeData = signal<TreeNode[]>([]);
-    selected: string[] = [];
+    selected = signal<string[]>([]);
+
+    fieldSnapshot = computed(() => {
+    const data = this.dataSnapshot();
+    const path = this.selected();
+
+    if (!data || path.length === 0) {
+        return [];
+    }
+
+    let current: any = data;
+
+    for (const key of path) {
+        current = current[key];
+
+        if (current == null) {
+            return [];
+        }
+    }
+
+    return current as ItemData[];
+});
 
     isSelected(node: TreeNode): boolean {
-        return node.path == this.selected;
+        return node.path == this.selected();
     }
 
     select(node: TreeNode) {
-        this.selected = node.path;
+        this.selected.set(node.path);
     }
 }
