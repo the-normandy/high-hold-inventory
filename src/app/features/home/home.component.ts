335c4465d66df9ad49ac656586a8b4946796b119
the-normandy <astarianott@gmail.com>
2026-07-06 22:26:35 -0300

    Refactor home screen for ease of navigation

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,16 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { RouterLink } from "@angular/router";
 import { UpdaterComponent } from "./updater.component";
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.component.html',
     styles: `:host { @apply flex-1; }`,
     imports: [
-        MatCardModule, MatButtonModule, RouterLink, UpdaterComponent
+        MatCardModule, MatButtonModule, RouterLink, UpdaterComponent,
+        MatDialogModule
     ]
 })
 export class HomeComponent {
+    dialog = inject(MatDialog);
+
+    dataSettings() {
+        // Implement
+    }
 }
