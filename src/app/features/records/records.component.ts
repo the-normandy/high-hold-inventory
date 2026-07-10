082eaa681c4ae8dc64c2f40713de1f608d5151cc
the-normandy <astarianott@gmail.com>
2026-07-09 23:11:47 -0300

    Add scaffolding components

---
+++ src/app/features/records/records.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-records',
+    templateUrl: 'records.component.html',
+    styles: `:host { @apply flex-1; }`
+})
+export class RecordsComponent {
+
+}
---
+++ src/app/features/records/records.model.ts
@@ -0,0 +1,16 @@
+type entryType = 'deposit' | 'withdrawal'
+
+export interface RecordEntry {
+    id: string;
+    entry: entryType;
+    timestamp: string;
+    items: RecordItem[];
+    totalValue: number;
+    note?: string;
+}
+
+export interface RecordItem {
+    name: string;
+    quantity: number;
+    value: number;
+}
