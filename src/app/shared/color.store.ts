2af5259aba36bea7c08076cc4a45f4fed44ee9f6
the-normandy <astarianott@gmail.com>
2026-07-11 18:23:21 -0300

    Add more colors to the ColorStore

--- src/app/shared/color.store.ts
+++ src/app/shared/color.store.ts
@@ -4,7 +4,7 @@ import { ThemeStore } from "../core/theme/theme.store";
 @Injectable({
     providedIn: 'root'
 })
-export class ColorsStore {
+export class ColorStore {
     theme = inject(ThemeStore);
     resolve = computed(() => {
         this.theme.isDark();
@@ -13,11 +13,14 @@ export class ColorsStore {
         return {
             primary: this.resolveCssColor('--mat-sys-primary'),
             secondary: this.resolveCssColor('--mat-sys-secondary'),
+            tertiary: this.resolveCssColor('--mat-sys-tertiary'),
             primaryContainer: this.resolveCssColor('--mat-sys-primary-container'),
             secondaryContainer: this.resolveCssColor('--mat-sys-secondary-container'),
+            tertiaryContainer: this.resolveCssColor('--mat-sys-tertiary-container'),
             surface: this.resolveCssColor('--mat-sys-surface'),
             onSurface: this.resolveCssColor('--mat-sys-on-surface'),
-            outline: this.resolveCssColor('--mat-sys-outline')
+            outline: this.resolveCssColor('--mat-sys-outline'),
+            outlineVariant: this.resolveCssColor('--mat-sys-outline-variant')
         }
     })
 
