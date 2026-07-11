f72dcadc78096626a223a39a53818e8e071bbfc9
the-normandy <astarianott@gmail.com>
2026-07-11 14:23:16 -0300

    Add access to records component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { InventoryComponent } from './features/inventory/inventory.component';
 import { DataComponent } from './features/data/data.component';
+import { RecordsComponent } from './features/records/records.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'new/:mode', component: InventoryComponent},
-    {path: 'data', component: DataComponent}
+    {path: 'data', component: DataComponent},
+    {path: 'records', component: RecordsComponent}
 ];
