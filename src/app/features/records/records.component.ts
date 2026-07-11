d3c7e6946e644166e0f48eba170903ad156c27f7
the-normandy <astarianott@gmail.com>
2026-07-10 22:17:59 -0300

    Add Material dependencies

--- src/app/features/records/records.component.ts
+++ src/app/features/records/records.component.ts
@@ -1,9 +1,16 @@
 import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatTableModule } from "@angular/material/table";
 
 @Component({
     selector: 'app-records',
     templateUrl: 'records.component.html',
-    styles: `:host { @apply flex-1; }`
+    styles: `:host { @apply flex-1; }`,
+    imports: [
+        MatTableModule, MatPaginatorModule, MatCardModule, MatButtonModule
+    ]
 })
 export class RecordsComponent {
 
