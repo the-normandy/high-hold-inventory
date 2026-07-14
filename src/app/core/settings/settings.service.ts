204a45a6a4adf04c025668f11e0c0eebcf3d38cd
the-normandy <astarianott@gmail.com>
2026-07-14 20:34:26 -0300

    Add settings service

---
+++ src/app/core/settings/settings.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, signal } from "@angular/core";
+import { BaseDirectory, readTextFile } from "@tauri-apps/plugin-fs";
+import { Settings } from "../data/settings.model";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SettingsService {
+
+    clan = signal<string>('');
+    character = signal<string>('');
+
+    async loadSettings() {
+        const text = await readTextFile('settings.json', {baseDir: BaseDirectory.AppLocalData});
+        const data = JSON.parse(text) as Settings;
+        this.clan.set(data.clan);
+        this.character.set(data.character);
+    }
+}
